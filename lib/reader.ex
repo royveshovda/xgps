c15cf075c13975fd34a5a1c1c1bb34ff53c89218
Roy Veshovda <roy@veshovda.no>
2016-07-22 15:28:08 +0300

    Use struct for state

--- lib/reader.ex
+++ lib/reader.ex
@@ -69,39 +69,80 @@ defmodule XGPS.Reader do
 
   ######
 
+  defmodule State do
+    defstruct [
+      gps_data: nil,
+      pid: nil,
+      port_name: nil,
+      data_buffer: nil
+    ]
+  end
+
+  defmodule Gps_state do
+    defstruct [
+      has_fix: false,
+      time: nil,
+      date: nil,
+      latitude: nil,
+      longitude: nil,
+      geoidheight: nil,
+      altitude: nil,
+      speed: nil,
+      angle: nil,
+      magvariation: nil,
+      hdop: nil,
+      fix_quality: nil,
+      satelites: nil,
+    ]
+  end
+
+  #uint8_t hour, minute, seconds, year, month, day;
+  #uint16_t milliseconds;
+  #float latitude, longitude;
+  #float geoidheight, altitude;
+  #float speed, angle, magvariation, HDOP;
+  #uint8_t fixquality, satellites;
+
   def init(port_name) do
     {:ok, pid} = Nerves.UART.start_link
     :ok = Nerves.UART.open(pid, port_name, speed: 9600, active: true)
-    {:ok, %{pid: pid, port_name: port_name, data: ""}}
+    gps_data = %Gps_state{has_fix: false}
+    state = %State{gps_data: gps_data, pid: pid, port_name: port_name, data_buffer: ""}
+    {:ok, state}
   end
 
-  def handle_info({:nerves_uart, port_name, "$"}, %{pid: pid, port_name: port_name, data: ""}) do
-    {:noreply, %{pid: pid, port_name: port_name, data: ""}}
+  # TODO: Use struct as state
+
+  def handle_info({:nerves_uart, _port_name, "$"}, %State{data_buffer: ""} = state) do
+    {:noreply, state}
   end
 
-  def handle_info({:nerves_uart, port_name, "$"}, %{pid: pid, port_name: port_name, data: data}) do
-    sentence = String.strip(("$" <> data))
+  def handle_info({:nerves_uart, _port_name, "$"}, state) do
+    sentence = String.strip(("$" <> state.data_buffer))
     # TODO: send to subscribers
     IO.puts(sentence)
     IO.inspect XGPS.Parser.parse_sentence(sentence)
-    {:noreply, %{pid: pid, port_name: port_name, data: ""}}
+
+    # TODO: Update state based on parse result
+    {:noreply, %{state | data_buffer: ""}}
   end
 
-  def handle_info({:nerves_uart, port_name, data}, %{pid: pid, port_name: port_name, data: old_data}) do
-    {:noreply, %{pid: pid, port_name: port_name, data: old_data <> data}}
+  def handle_info({:nerves_uart, _port_name, data}, state) do
+    data_buffer = state.data_buffer <> data
+    {:noreply, %{state | data_buffer: data_buffer}}
   end
 
   def handle_call(:stop, _from, state) do
     {:stop, :normal, state}
   end
 
-  def handle_cast({:command, command}, %{pid: pid, port_name: _port_name, data: _data} = state) do
-    Nerves.UART.write(pid, (command <> "\r\n"))
+  def handle_cast({:command, command}, state) do
+    Nerves.UART.write(state.pid, (command <> "\r\n"))
     {:noreply, state}
   end
 
-  def terminate(_reason, %{pid: pid, port_name: _port_name}) do
-    Nerves.UART.close(pid)
-    Nerves.UART.stop(pid)
+  def terminate(_reason, state) do
+    Nerves.UART.close(state.pid)
+    Nerves.UART.stop(state.pid)
   end
 end
