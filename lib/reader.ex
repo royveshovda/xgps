5c51a6494986910fc082513225de8cd4045357c9
Roy Veshovda <roy@veshovda.no>
2016-07-22 08:42:21 +0300

    Send commands to GPS

--- lib/reader.ex
+++ lib/reader.ex
@@ -1,7 +1,7 @@
 defmodule XGPS.Reader do
   use GenServer
 
-  # Onlu for debugging
+  # Only for debugging
   def serial0() do
     start_link("/dev/serial0")
   end
@@ -27,10 +27,51 @@ defmodule XGPS.Reader do
     :ok
   end
 
+  def command_output_off do
+    cmd = "$PMTK314,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
+    send_command(cmd)
+  end
+
+  def command_output_all_data do
+    cmd = "$PMTK314,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
+    send_command(cmd)
+  end
+
+  def command_output_rmc_gga do
+    cmd = "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
+    send_command(cmd)
+  end
+
+  def command_output_rmc_only do
+    cmd = "$PMTK314,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*29"
+    send_command(cmd)
+  end
+
+  def command_ask_for_version do
+    cmd = "$PMTK605*31"
+    send_command(cmd)
+  end
+
+  def command_antenna_on do
+    cmd = "$PGCMD,33,1*6C"
+    send_command(cmd)
+  end
+
+  def command_antenna_off do
+    cmd = "$PGCMD,33,0*6D"
+    send_command(cmd)
+  end
+
+  defp send_command(command) do
+    pid = Process.whereis(__MODULE__)
+    GenServer.cast(pid, {:command, command})
+  end
+
+  ######
+
   def init(port_name) do
     {:ok, pid} = Nerves.UART.start_link
     :ok = Nerves.UART.open(pid, port_name, speed: 9600, active: true)
-    Nerves.UART.write(pid, "$PGCMD,33,1*6C\r\n")
     {:ok, %{pid: pid, port_name: port_name, data: ""}}
   end
 
@@ -41,7 +82,7 @@ defmodule XGPS.Reader do
   def handle_info({:nerves_uart, port_name, "$"}, %{pid: pid, port_name: port_name, data: data}) do
     sentence = String.strip(("$" <> data))
     # TODO: send to subscribers
-    # IO.puts(sentence)
+    IO.puts(sentence)
     IO.inspect XGPS.Parser.parse_sentence(sentence)
     {:noreply, %{pid: pid, port_name: port_name, data: ""}}
   end
@@ -52,6 +93,11 @@ defmodule XGPS.Reader do
 
   def handle_call(:stop, _from, state) do
     {:stop, :normal, state}
+  end
+
+  def handle_cast({:command, command}, %{pid: pid, port_name: _port_name, data: _data} = state) do
+    Nerves.UART.write(pid, (command <> "\r\n"))
+    {:noreply, state}
   end
 
   def terminate(_reason, %{pid: pid, port_name: _port_name}) do
