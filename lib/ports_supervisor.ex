106563bad3cba2686e9b3b5017f826d22ec095ea
Roy Veshovda <roy@veshovda.no>
2016-07-24 08:40:27 +0300

    Start port if config set

--- lib/ports_supervisor.ex
+++ lib/ports_supervisor.ex
@@ -2,7 +2,15 @@ defmodule XGPS.Ports_supervisor do
   use Supervisor
 
   def start_link do
-    Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+    result = {:ok, pid} = Supervisor.start_link(__MODULE__, :ok, name: __MODULE__)
+    # Start port if defined in config
+    case Application.get_env(:xgps, :port_to_start, :no_port_to_start) do
+      :no_port_to_start ->
+        :ok
+      portname_with_args ->
+        start_port_with_args(pid, portname_with_args)
+    end
+    result
   end
 
   def start_port(port_name) do
@@ -19,15 +27,6 @@ defmodule XGPS.Ports_supervisor do
     children = [
       worker(XGPS.Port.Supervisor, [], restart: :transient)
     ]
-
-    # Start port if defined in config
-    case Application.get_env(:xgps, :port_to_start, :no_port_to_start) do
-      :no_port_to_start ->
-        supervise(children, strategy: :simple_one_for_one, max_restarts: 3, max_seconds: 5)
-      portname_with_args ->
-        with {:ok, sup_pid} <- supervise(children, strategy: :simple_one_for_one, max_restarts: 3, max_seconds: 5),
-              :ok <- XGPS.Ports_supervisor.start_port_with_args(portname_with_args),
-        do: {:ok, sup_pid}
-    end
+    supervise(children, strategy: :simple_one_for_one)
   end
 end
