fca3eaf04f2e328f08e99ba588eff95cb15b4efd
Roy Veshovda <roy@veshovda.no>
2016-07-24 05:45:39 +0300

    Require Elixir 1.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule XGPS.Mixfile do
     [app: :xgps,
      name: XGPS,
      version: "0.1.0",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      source_url: "https://github.com/rveshovda/xgps",
