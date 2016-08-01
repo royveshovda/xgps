f4de728fd661ab4fd66d84fcffd8d62416323612
Roy Veshovda <roy@veshovda.no>
2016-08-01 08:59:51 +0300

    Simulator from file

--- lib/xgps/port/simulator.ex
+++ lib/xgps/port/simulator.ex
@@ -12,11 +12,37 @@ defmodule XGPS.Port.Simulator do
       ]
   end
 
-  def init(file_name) do
-    # TODO: Open file
-    state = %State{file_name: file_name, positions: []}
+  def init({file_name}) do
+    {:ok, content} = File.read(file_name)
+    positions =
+      content
+      |> String.split(["\n", "\r\n"])
+      |> Enum.filter(&(not String.starts_with?(&1,"#")))
+      |> Enum.filter(&(String.length(&1) > 0))
+      |> Enum.map(&(String.split(&1, ",")))
+      |> Enum.map(&(parse_line(&1)))
+
+    state = %State{file_name: file_name, positions: positions}
+
     IO.puts "Simulator running with initial state: #{inspect state}"
     {:ok, state}
   end
 
+  defp parse_line(line_pieces) do
+    pieces = Enum.map(line_pieces, &(String.trim(&1)))
+    parse_line_pieces(pieces)
+  end
+
+  defp parse_line_pieces([lat_s, lon_s]) do
+    {lat, _} = Float.parse(lat_s)
+    {lon, _} = Float.parse(lon_s)
+    {lat, lon}
+  end
+
+  defp parse_line_pieces([lat_s, lon_s, timestamp_s]) do
+    {lat, _} = Float.parse(lat_s)
+    {lon, _} = Float.parse(lon_s)
+    {:ok, timestamp} = NaiveDateTime.from_iso8601(timestamp_s)
+    {lat, lon, timestamp}
+  end
 end
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule XGPS.Mixfile do
   def project do
     [app: :xgps,
      name: XGPS,
-     version: "0.3.1",
+     version: "0.4.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -36,7 +36,7 @@ defmodule XGPS.Mixfile do
   defp package do
     [# These are the default files included in the package
      name: :xgps,
-     files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
+     files: ["lib", "priv", "mix.exs", "README*", "LICENSE*", "simulator_positions.txt"],
      maintainers: ["Roy Veshovda"],
      licenses: ["MIT"],
      links: %{"GitHub" => "https://github.com/rveshovda/xgps"}]
