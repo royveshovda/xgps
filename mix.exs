32c65fb9000992c509120ae75ec47f41fdea8a19
Roy Veshovda <roy@veshovda.no>
2022-10-01 13:48:51 +0200

    relax versions

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule XGPS.Mixfile do
     [app: :xgps,
      name: XGPS,
      version: "0.5.0",
-     elixir: "~> 1.10.4",
+     elixir: "~> 1.10",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      source_url: "https://github.com/royveshovda/xgps",
