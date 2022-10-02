6d5761623641d9e60288783be5763825cd729fe5
Roy Veshovda <roy@veshovda.no>
2022-10-02 06:38:18 +0200

    disable by default

--- config/dev.exs
+++ config/dev.exs
@@ -2,6 +2,6 @@ import Config
 
 #config :xgps, port_to_start: {"/dev/ttyUSB0", "AdafruitGps"}
 #config :xgps, port_to_start: {:simulate, "simulator_positions.txt"}
-config :xgps, port_to_start: {:simulate}
+#config :xgps, port_to_start: {:simulate}
 
 config :logger, level: :info
