54e52ba738ace3dc3f5202e02cce2d1754bc7a59
Roy Veshovda <roy@veshovda.no>
2022-10-06 06:52:54 +0200

    Simplify config

--- config/dev.exs
+++
@@ -1,7 +0,0 @@
-import Config
-
-#config :xgps, port_to_start: {"/dev/ttyUSB0", "AdafruitGps"}
-#config :xgps, port_to_start: {:simulate, "simulator_positions.txt"}
-#config :xgps, port_to_start: {:simulate}
-
-config :logger, level: :info
