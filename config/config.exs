c569e8c34de60d2e2281bed2cee9ba928d6007a8
Roy Veshovda <roy@veshovda.no>
2022-10-08 13:01:21 +0200

    Update documentation

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,3 @@
 import Config
-#config :xgps, port_to_start: [port_name: :simulate, file_name: "simulator_positions.txt"]
-#config :xgps, port_to_start: [port_name: "/dev/ttyUSB0", driver: "PMTK"]
-config :logger, level: :debug
+
+config :logger, level: :info
